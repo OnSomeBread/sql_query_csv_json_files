9b93856aa57046743106ae33d7d672449f8c8556
OnSomeBread <mark1qaz@comcast.net>
2026-01-17 12:14:23 -0600

    changed entire program to be a cli tool making it easier to read and interact with output

---
+++ src/app.rs
@@ -0,0 +1,207 @@
+use arrow::array::RecordBatch;
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use datafusion::{arrow::array::ArrayRef, prelude::SessionContext};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+    style::{Color, Style, Stylize},
+    widgets::{Block, Borders, Cell, Paragraph, Row, Table},
+};
+use rfd::FileDialog;
+use std::time::Duration;
+
+use datafusion::execution::context::SessionConfig;
+use rayon::prelude::*;
+
+use crate::build_ctx;
+pub struct App {
+    curr_headers: Vec<String>,
+    curr_command: String,
+    curr_data: Vec<Vec<String>>,
+    shown_data: (usize, usize),
+    scroll_offset: (usize, usize),
+    next_table_name: u8,
+    ctx: SessionContext,
+}
+
+impl App {
+    pub fn new() -> Self {
+        let config = SessionConfig::new().with_information_schema(true);
+        Self {
+            curr_headers: vec![],
+            curr_command: String::new(),
+            curr_data: vec![],
+            shown_data: (20, 8),
+            scroll_offset: (0, 0),
+            next_table_name: b'a',
+            ctx: SessionContext::new_with_config(config),
+        }
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        let layout = Layout::default()
+            .direction(ratatui::layout::Direction::Vertical)
+            .constraints(vec![Constraint::Percentage(85), Constraint::Percentage(15)])
+            .split(frame.area());
+
+        let headers = Row::new(
+            self.curr_headers
+                .iter()
+                .skip(self.scroll_offset.1)
+                .take(self.scroll_offset.1 + self.shown_data.1)
+                .cloned(),
+        )
+        .style(Style::new().bold().fg(Color::LightBlue));
+
+        let rows = self
+            .curr_data
+            .iter()
+            .skip(self.scroll_offset.0)
+            .take(self.scroll_offset.0 + self.shown_data.0)
+            .map(|item| {
+                Row::new(
+                    item.iter()
+                        .skip(self.scroll_offset.1)
+                        .take(self.scroll_offset.1 + self.shown_data.1)
+                        .map(|x| Cell::from(x.clone())),
+                )
+                .style(Style::new().fg(Color::White))
+            });
+
+        frame.render_widget(
+            Table::new(
+                rows,
+                (0..self.shown_data.1)
+                    .map(|_| Constraint::Percentage((100 / self.shown_data.1) as u16)),
+            )
+            .header(headers)
+            .block(Block::new().fg(Color::LightBlue).borders(Borders::ALL)),
+            layout[0],
+        );
+
+        // command input
+        frame.render_widget(
+            Paragraph::new(self.curr_command.clone())
+                .block(Block::new().fg(Color::LightBlue).borders(Borders::ALL)),
+            layout[1],
+        );
+    }
+
+    pub async fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        loop {
+            terminal.draw(|frame| self.render(frame))?;
+            if event::poll(Duration::from_millis(20))?
+                && let Event::Key(key) = event::read()?
+                && key.kind == KeyEventKind::Press
+            {
+                if key.code == KeyCode::Esc {
+                    return Ok(());
+                } else if key.code == KeyCode::Up {
+                    self.scroll_offset.0 = self.scroll_offset.0.saturating_sub(2);
+                } else if key.code == KeyCode::Down {
+                    self.scroll_offset.0 = self.scroll_offset.0.saturating_add(2);
+                } else if key.code == KeyCode::Left {
+                    self.scroll_offset.1 = self.scroll_offset.1.saturating_sub(1);
+                } else if key.code == KeyCode::Right {
+                    self.scroll_offset.1 = self.scroll_offset.1.saturating_add(1);
+                } else if key.code == KeyCode::Enter {
+                    self.parse_command().await?;
+                } else if key.code == KeyCode::Backspace {
+                    let _ = self.curr_command.pop();
+                } else if let Some(k) = key.code.as_char() {
+                    self.curr_command.push(k);
+                }
+            }
+        }
+    }
+
+    fn send_message(&mut self, error: String) {
+        self.curr_headers = vec![];
+        self.curr_data = vec![vec![error]];
+        self.scroll_offset = (0, 0);
+    }
+
+    fn read_batch_to_array(batch: &RecordBatch) -> Vec<Vec<String>> {
+        let columns: Vec<ArrayRef> = batch.columns().to_vec();
+        let num_rows = batch.num_rows();
+        let mut rows = Vec::with_capacity(num_rows);
+
+        for row in 0..num_rows {
+            let mut values = Vec::with_capacity(columns.len());
+
+            for col in &columns {
+                let value = if col.is_null(row) {
+                    "null".to_string()
+                } else {
+                    arrow::util::display::array_value_to_string(col.as_ref(), row).unwrap()
+                };
+                values.push(value);
+            }
+
+            rows.push(values);
+        }
+
+        rows
+    }
+
+    async fn parse_command(&mut self) -> Result<()> {
+        if self.curr_command.trim().eq_ignore_ascii_case("c") {
+            self.create_table().await?;
+        } else if self.curr_command.trim().eq_ignore_ascii_case("cls") {
+            self.curr_data = vec![];
+        } else {
+            match self.ctx.sql(self.curr_command.trim_end()).await {
+                Ok(df) => {
+                    self.curr_headers = vec![];
+                    for field in df.schema().fields() {
+                        self.curr_headers.push(field.name().clone());
+                    }
+
+                    match df.collect().await {
+                        Ok(batches) => {
+                            self.curr_data = batches
+                                .par_iter()
+                                .map(Self::read_batch_to_array)
+                                .flatten()
+                                .collect();
+                        }
+                        Err(e) => self.send_message(e.to_string()),
+                    }
+                }
+                Err(e) => self.send_message(e.to_string()),
+            }
+        }
+
+        self.curr_command = String::new();
+        self.scroll_offset = (0, 0);
+        Ok(())
+    }
+
+    pub async fn create_table(&mut self) -> Result<()> {
+        // user selects a view to view
+        let Some(file_location) = FileDialog::new()
+            .set_title("Select a CSV or JSON File")
+            .add_filter(".csv or .json", &["csv", "json"])
+            .pick_file()
+        else {
+            self.send_message("could not open file".to_string());
+            return Ok(());
+        };
+
+        build_ctx::add_file_to_ctx(
+            &self.ctx,
+            char::from(self.next_table_name).to_string(),
+            file_location.into_os_string().into_string().unwrap(),
+        )
+        .await?;
+
+        self.send_message(format!(
+            "created table {}",
+            char::from(self.next_table_name)
+        ));
+        self.next_table_name += 1;
+
+        Ok(())
+    }
+}
--- src/build_ctx.rs
+++ src/build_ctx.rs
@@ -1,4 +1,4 @@
-use anyhow::{Result, anyhow};
+use color_eyre::{Result, eyre::eyre};
 use datafusion::prelude::*;
 use std::io::Write;
 pub async fn add_file_to_ctx(
@@ -37,16 +37,14 @@ pub async fn add_file_to_ctx(
                     ctx.register_json(table_name.clone(), new_file, NdJsonReadOptions::default())
                         .await?;
                 } else {
-                    return Err(anyhow!("could not parse from json array or ndjson"));
+                    return Err(eyre!("could not parse from json array or ndjson"));
                 }
             }
         } else {
-            return Err(anyhow!("cannot parse this file format"));
+            return Err(eyre!("cannot parse this file format"));
         }
     } else {
-        return Err(anyhow!("could not parse file format"));
+        return Err(eyre!("could not parse file format"));
     }
-
-    tracing::info!("created table {}", table_name);
     Ok(())
 }
